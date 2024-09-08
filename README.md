67ba9f83930b23463e17264f3458437f214afe72
Nathanael Connell <nathanael@Nathanaels-MacBook-Pro.local>
2024-09-08 12:41:22 -0600

    First commit

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ not there.
 
 Use the template in `code.js`. The tests in `code.test.js` will be run
 automatically every time you push to Github; if they pass you are done.
+
+I used the help of github to find the middle of the list and how to use the Math.floor to be able to get the right element in the list
--- code.js
+++ code.js
@@ -1,3 +1,19 @@
 function binarySearch(list, element) {
-    return -1;
+    if (list.length != 0){
+        var low = 0;
+        var high = list.length - 1;
+        while (low <= high) {
+            var mid = Math.floor((low + high) / 2);
+            if (list[mid] === element) {
+                return mid;
+            }
+            else if (list[mid] < element) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+    }
+    else{return -1;}
 }
