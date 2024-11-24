883e9f09df35797fc26c0826adbc4764452e6be2
Nathanael Connell <nathanael@Nathanaels-MacBook-Pro.local>
2024-11-23 17:11:32 -0700

    Fixed to run on github

--- code.js
+++ code.js
@@ -1,18 +1,19 @@
 function binarySearch(arr, target) {
     let left = 0;
     let right = arr.length - 1;
+    let result = -1;
 
     while (left <= right) {
         const mid = Math.floor((left + right) / 2);
-
         if (arr[mid] === target) {
-            return mid; 
+            result = mid; 
+            right = mid - 1; 
         } else if (arr[mid] < target) {
             left = mid + 1; 
         } else {
-            right = mid - 1;
+            right = mid - 1; 
         }
     }
 
-    return -1; 
+    return result;
 }
